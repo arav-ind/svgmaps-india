6c459fe47e6f570d19cd278d31f26a381fe84144
Aravind D <aravindrndv8@gmail.com>
2026-02-04 20:26:19 +0530

    feat: gujarat

---
+++ packages/gujarat/src/components/Gujarat.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import RenderSvg from 'svgmap-core'
+import { MapPropType } from '../types/types'
+import { districtCodes, drawPath } from '../constants/gujarat'
+
+const GujaratMap = ({
+    className = 'svgmap',
+    size,
+    mapColor,
+    strokeColor,
+    strokeWidth,
+    hoverColor,
+    onClick,
+}: MapPropType
+) => {
+    return (
+        <RenderSvg
+            districtCodes={districtCodes}
+            drawPath={drawPath}
+            onClick={onClick}
+            className={className}
+            size={size}
+            mapColor={mapColor}
+            strokeColor={strokeColor}
+            strokeWidth={strokeWidth}
+            hoverColor={hoverColor}
+            viewBox="0 0 1000 719.61"
+        />
+    )
+}
+export default GujaratMap
---
+++ packages/stories/src/Gujarat.stories.tsx
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import GujaratMap, { districtNames } from "../../gujarat/src/index";
+
+export const Default = () => {
+  const [selectedValue, setSelectedValue] = useState<string | null>(null);
+
+  return (
+    <div>
+      <h3>
+        {selectedValue
+          ? `${selectedValue}: ${districtNames[selectedValue]}`
+          : "None"}
+      </h3>
+      <GujaratMap
+        onClick={(value: string) => setSelectedValue(value)}
+        size="600px"
+        mapColor="white"
+        strokeColor="black"
+        strokeWidth="2"
+        hoverColor="rgb(80 80 80)"
+      />
+    </div>
+  );
+};
+
+Default.storyName = "Gujarat";

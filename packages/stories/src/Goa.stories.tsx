5d21fc9be8e6b7b306332b7296ae48269b6bb2e2
Aravind D <aravindrndv8@gmail.com>
2026-02-06 18:20:57 +0530

    feat: add Goa

---
+++ packages/stories/src/Goa.stories.tsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react'
+import GoaMap, { districtNames } from '../../goa/src/index'
+
+export const Default = () => {
+    const [selectedValue, setSelectedValue] = useState<string | null>(null)
+
+    return (
+        <div>
+            <h3>{selectedValue ? `${selectedValue}: ${districtNames[selectedValue]}` : 'None'}</h3>
+            <GoaMap
+                onClick={(value: string) => setSelectedValue(value)}
+                size='300px'
+                mapColor='white'
+                strokeColor='black'
+                strokeWidth='1'
+                hoverColor='rgb(80 80 80)'
+            />
+        </div>
+    )
+}
+
+Default.storyName = 'Goa'

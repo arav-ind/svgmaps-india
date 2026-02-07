e55b73820881d1e9b5d2d384caf58583c321d498
Aravind D <aravindrndv8@gmail.com>
2026-02-07 15:00:30 +0530

    feat: Kashmir

---
+++ packages/__stories__/src/Kashmir.stories.tsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react'
+import KashmirMap, { districtNames } from '../../kashmir/src/index'
+
+export const Default = () => {
+    const [selectedValue, setSelectedValue] = useState<string | null>(null)
+
+    return (
+        <div>
+            <h3>{selectedValue ? `${selectedValue}: ${districtNames[selectedValue]}` : 'None'}</h3>
+            <KashmirMap
+                onClick={(value: string) => setSelectedValue(value)}
+                size='400px'
+                mapColor='white'
+                strokeColor='black'
+                strokeWidth='1'
+                hoverColor='rgb(80 80 80)'
+            />
+        </div>
+    )
+}
+
+Default.storyName = 'Kashmir'

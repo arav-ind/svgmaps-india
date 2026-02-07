485bb6d5d9c9296e41fda4b1f4f7429f4533f0d4
Aravind D <aravindrndv8@gmail.com>
2026-02-07 14:08:29 +0530

    feat: Punjab

---
+++ packages/__stories__/src/Punjab.stories.tsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react'
+import PunjabMap, { districtNames } from '../../punjab/src/index'
+
+export const Default = () => {
+    const [selectedValue, setSelectedValue] = useState<string | null>(null)
+
+    return (
+        <div>
+            <h3>{selectedValue ? `${selectedValue}: ${districtNames[selectedValue]}` : 'None'}</h3>
+            <PunjabMap
+                onClick={(value: string) => setSelectedValue(value)}
+                size='400px'
+                mapColor='white'
+                strokeColor='black'
+                strokeWidth='1'
+                hoverColor='rgb(80 80 80)'
+            />
+        </div>
+    )
+}
+
+Default.storyName = 'Punjab'
